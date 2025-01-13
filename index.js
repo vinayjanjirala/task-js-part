370f40651897a3ec7bf64206f05dcadd363542e1
vinayjanjirala <vinayjanjirala7@gmail.com>
2025-01-13 21:32:38 +0530

    js code

---
+++ index.js
@@ -0,0 +1,94 @@
+var div1 = document.createElement('div');    
+div1.className= "container";
+var div2=document.createElement("div");
+div2.className= "contain";
+var div3=document.createElement("div");
+div3.className= "contain";
+var div4=document.createElement("div");
+div4.className= "contain";
+var div5=document.createElement("div");
+div5.className= "contain";
+var div6=document.createElement("div");
+div6.className= "contain";
+var div7=document.createElement("div");
+div7.className= "contain";
+var div8=document.createElement("div");
+div8.className= "contained";
+var div9=document.createElement("div");
+div9.className= "contained";
+var div10=document.createElement("div");
+div10.className= "contained";
+var div11=document.createElement("div");
+div11.className= "contained";
+var div12=document.createElement("div");
+div12.className= "contained";
+var div13=document.createElement("div");
+div13.className= "contained";
+div2.appendChild(div8);
+div3.appendChild(div9);
+div4.appendChild(div10);
+div5.appendChild(div11);
+div6.appendChild(div12);
+div7.appendChild(div13);
+div1.append(div2,div3,div3,div4,div5,div6,div7);
+document.body.appendChild(div1)
+var img1=document.createElement("img");
+img1.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF70ezOPc9X-w7njdQomNjQx76-_pig7Ydgw&s';
+var img2=document.createElement("img");
+img2.src ='https://www.cubesnjuliennes.com/wp-content/uploads/2021/03/Best-Mutton-Biryani-Recipe.jpg';
+var img3=document.createElement("img");
+img3.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqENjdQe-Mm5gTmtkv3DN9j0KxTTrJKu3qJw&s';
+var img4=document.createElement("img");
+img4.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTrfDoKVTRHHDocaqROx1jE822MuOHlx0Qog&s';
+var img5=document.createElement("img");
+img5.src ='https://i0.wp.com/www.asheescookbook.com/wp-content/uploads/2015/11/20180404_165220.jpg';
+var img6=document.createElement("img");
+img6.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaULrDzejMQMqWTNqOPk1SmuBZ2Q_8C9jmAw&s';
+div8.appendChild(img1);
+div9.appendChild(img2);
+div10.appendChild(img3);
+div11.appendChild(img4);
+div12.appendChild(img5);
+div13.appendChild(img6);
+var h1=document.createElement("h3")
+h1.innerText="Chicken Biryani"
+div2.appendChild(h1)
+var h2=document.createElement("h3")
+h2.innerText="Mutton Biryani"
+div3.appendChild(h2)
+var h3=document.createElement("h3")
+h3.innerText="Manchuriya"
+div4.appendChild(h3)
+var h4=document.createElement("h3")
+h4.innerText="Chicken Lollipop"
+div5.appendChild(h4)
+var h5=document.createElement("h3")
+h5.innerText="Chicken Majestic"
+div6.appendChild(h5)
+var h6=document.createElement("h3")
+h6.innerText="Chicken 65"
+div7.appendChild(h6)
+var p1=document.createElement("p")
+p1.innerText="Price: 170rs"
+div2.appendChild(p1)
+var p2=document.createElement("p")
+p2.innerText="Price: 200rs"
+div3.appendChild(p2)
+var p3=document.createElement("p")
+p3.innerText="Price: 120rs"
+div4.appendChild(p3)
+var p4=document.createElement("p")
+p4.innerText="Price: 140rs"
+div5.appendChild(p4)
+var p5=document.createElement("p")
+p5.innerText="Price: 130rs"
+div6.appendChild(p5)
+var p6=document.createElement("p")
+p6.innerText="Price: 140rs"
+div7.appendChild(p6)
+
+
+
+
+
+
